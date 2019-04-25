7b2ca5a624eb585d8fc17fc15483c69a393aca5f
Anatolii Zetilov <azetilov@gmail.com>
2019-04-25 23:21:24 +0300

    feat(adChannels): implement update and delete

--- Ads.Api/Controllers/AdChannelsController.cs
+++ Ads.Api/Controllers/AdChannelsController.cs
@@ -76,29 +76,49 @@ namespace Ads.Api.Controllers
         }
         
         /// <summary>
-        /// Returns advertisement channels by Id
+        /// Updates advertisement channel by Id
         /// </summary>
-        /// <response code="200">Advertisement channels</response>
+        /// <response code="200">Advertisement channel</response>
         /// <response code="404">Advertisement channel not found</response>
         /// <returns>Advertisement channel</returns>
-        [HttpGet("{id:long}")]
-        public ActionResult<AdChannelRepresentation> Update([FromRoute] long adId, [FromRoute] long id)
+        [HttpPut("{id:long}")]
+        public StatusCodeResult Update(
+            [FromRoute] long adId, 
+            [FromRoute] long id, 
+            [FromBody] AdChannelRepresentation channel)
         {
-            var adChannel = _context.AdChannels.AsNoTracking()
-                .Where(a => a.Id == id && a.AdId == adId)
-                .Select(a => new AdChannelRepresentation()
-                {
-                    Id = a.Id,
-                    Name = a.Name,
-                    AdId = a.AdId,
-                    ChannelId = a.ChannelId
-                })
-                .FirstOrDefault();
+            var adChannel = _context.AdChannels.FirstOrDefault(a => a.Id == id && a.AdId == adId);
+            
             if (adChannel == null)
             {
                 return NotFound();
             }
-            return adChannel;
+            adChannel.Name = channel.Name;
+            _context.AdChannels.Update(adChannel);
+            _context.SaveChanges();
+            return Ok();
+        }
+        
+        /// <summary>
+        /// Deletes advertisement channel by Id
+        /// </summary>
+        /// <response code="200">Advertisement channel</response>
+        /// <response code="404">Advertisement channel not found</response>
+        /// <returns>Advertisement channel</returns>
+        [HttpDelete("{id:long}")]
+        public StatusCodeResult Update(
+            [FromRoute] long adId, 
+            [FromRoute] long id)
+        {
+            var adChannel = _context.AdChannels.FirstOrDefault(a => a.Id == id && a.AdId == adId);
+            
+            if (adChannel == null)
+            {
+                return NotFound();
+            }
+            _context.AdChannels.Remove(adChannel);
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
